27e6d0d5d1394137ae3fdb802ce2ce7ce58e3d3b
Shayla White <shayladwhite1@gmail.com>
2020-01-13 05:00:47 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -18,10 +18,12 @@ GROUP BY pledges.user_id ORDER BY name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT Projects.title, SUM(amount) - Projects.funding_goal FROM projects, pledges 
-WHERE pledges.project_id = projects.id GROUP BY Projects.title 
-HAVING projects.id = pledges.project_id ORDER BY SUM(amount), 
-Projects.funding_goal > -1 DESC LIMIT 2;"
+"SELECT title, SUM(pledges.amount) - funding_goal FROM
+ projects
+  JOIN pledges
+  ON projects.id = pledges.project_id
+  GROUP BY project_id
+  HAVING SUM(pledges.amount) >= projects.funding_goal;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
